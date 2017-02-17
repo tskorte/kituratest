d3de77e54e18ee3a6feb1341e484ea5125236944
Karl Forfang <karl@forfang.no>
2017-02-17 18:20:25 +0100

    Added json calls

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,17 +1,45 @@
 import Kitura
+import KituraNet
 import HeliumLogger
+import SwiftyJSON
 
 HeliumLogger.use()
 let router = Router()
+let cabins: [Int: Any] = [1 :
+		[
+			"name":"Taumevatn Turistforeningshytte",
+			"serviceLevel": "Selvbetjent"
+		],
+	    2:
+		[
+			"name":"Storavatn",
+			"serviceLevel": "Selvbetjent"
+		],
+	    3:
+		[
+			"name": "Grautheller",
+			"serviceLevel": "Selvbetjent"
+		]
+	   ]
+
 router.get("/") { req, res, next in
-	res.send("Hello world")
+	let response = JSON(cabins)
+	res.send(response.rawString() ?? "No json")
 	next()
 }
 
-router.post("/error") { request, response, next in
-	let error = try request.readString() ?? ""
-	print(error)
-	response.send("Error received")
+router.post("/id/:id") { request, response, next in
+    var statusCode = HTTPStatusCode.notFound
+	var responseString = "Not found"
+	if let id = try request.parameters["name"] as? Int {
+		let found = cabins[id]
+		let json = JSON(found)
+		if let jsonString = json.rawString(){
+			response.status(.OK).send(json: json)
+		}
+	}
+    response.status(statusCode)
+	response.send(responseString)
 	next()	
 }
 
